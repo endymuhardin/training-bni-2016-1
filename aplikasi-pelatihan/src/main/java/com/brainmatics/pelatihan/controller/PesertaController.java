4d8e3ea5d8780525c6f307b8313e536d740d1806
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-07 21:08:05 +0700

    memasang layout dengan thymeleaf

---
+++ aplikasi-pelatihan/src/main/java/com/brainmatics/pelatihan/controller/PesertaController.java
@@ -0,0 +1,11 @@
+package com.brainmatics.pelatihan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PesertaController {
+    
+    @RequestMapping("/peserta/registrasi/")
+    public void registrasi(){}
+}
