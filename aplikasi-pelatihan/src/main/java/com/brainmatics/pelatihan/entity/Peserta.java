05535324e84ca2c299817a4283d866628d2812ee
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-14 19:09:19 +0700

    menambah field tanggalLahir

--- aplikasi-pelatihan/src/main/java/com/brainmatics/pelatihan/entity/Peserta.java
+++ aplikasi-pelatihan/src/main/java/com/brainmatics/pelatihan/entity/Peserta.java
@@ -1,11 +1,15 @@
 package com.brainmatics.pelatihan.entity;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -26,4 +30,9 @@ public class Peserta {
     
     @Column(name = "nomor_handphone")
     private String nomorHandphone;
+    
+    @NotNull @Past
+    @Column(name = "tanggal_lahir")
+    @Temporal(TemporalType.DATE)
+    private Date tanggalLahir;
 }
