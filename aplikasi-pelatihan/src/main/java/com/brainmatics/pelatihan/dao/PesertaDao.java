cfd8cfdc8ddbaebebac1731440e1230f7b2c0b2c
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-14 20:48:08 +0700

    contoh query method

--- aplikasi-pelatihan/src/main/java/com/brainmatics/pelatihan/dao/PesertaDao.java
+++ aplikasi-pelatihan/src/main/java/com/brainmatics/pelatihan/dao/PesertaDao.java
@@ -1,8 +1,10 @@
 package com.brainmatics.pelatihan.dao;
 
 import com.brainmatics.pelatihan.entity.Peserta;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface PesertaDao extends PagingAndSortingRepository<Peserta, String>{
-    
+    public Page<Peserta> findByNamaContaining(String keyword, Pageable page);
 }
