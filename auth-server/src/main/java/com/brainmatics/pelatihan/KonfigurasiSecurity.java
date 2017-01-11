ff54d1b0f9ab397b1ecd3303bbf5ca49785b0cd7
Endy Muhardin <endy.muhardin@gmail.com>
2017-01-11 19:55:43 +0700

    aplikasi auth server

---
+++ auth-server/src/main/java/com/brainmatics/pelatihan/KonfigurasiSecurity.java
@@ -0,0 +1,124 @@
+package com.brainmatics.pelatihan;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@EnableWebSecurity
+public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter {
+
+    private static final String SQL_LOGIN = "select username, password, active as enabled "
+            + "from s_user where username = ?";
+    private static final String SQL_PERMISSION = "select u.username as username, p.nama as authority "
+            + "from s_user u "
+            + "inner join s_user_role ur on u.id = ur.id_user "
+            + "inner join s_role r on ur.id_role = r.id "
+            + "inner join s_role_permission rp on rp.id_role = r.id "
+            + "inner join s_permission p on rp.id_permission = p.id "
+            + "where u.username = ?";
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .and()
+                .formLogin()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(SQL_LOGIN)
+                .authoritiesByUsernameQuery(SQL_PERMISSION)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class AuthorizationServerConfiguration extends
+            AuthorizationServerConfigurerAdapter {
+
+        private static final String RESOURCE_ID = "aplikasitraining";
+        
+        @Autowired
+        @Qualifier("authenticationManagerBean")
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+                throws Exception {
+            endpoints
+                    .tokenStore(new InMemoryTokenStore())
+                    .authenticationManager(authenticationManager);
+        }
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+            oauthServer.checkTokenAccess("hasRole('CLIENT')");
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients
+                    .inMemory()
+                    .withClient("clientauthcode")
+                    .secret("123456")
+                    .authorizedGrantTypes("authorization_code", "refresh_token")
+                    .scopes("read", "write")
+                    .resourceIds(RESOURCE_ID)
+                    .and()
+                    .withClient("clientcred")
+                    .secret("123456")
+                    .authorizedGrantTypes("client_credentials")
+                    .scopes("trust")
+                    .resourceIds(RESOURCE_ID)
+                    .and()
+                    .withClient("clientapp")
+                    .secret("123456")
+                    .authorizedGrantTypes("password")
+                    .scopes("read", "write")
+                    .resourceIds(RESOURCE_ID)
+                    .and()
+                    .withClient("jsclient")
+                    .secret("jspasswd")
+                    .authorizedGrantTypes("implicit")
+                    .scopes("read", "write")
+                    .resourceIds(RESOURCE_ID)
+                    .authorities("CLIENT")
+                    .redirectUris("http://localhost:20000/implicit-client/")
+                    .accessTokenValiditySeconds(60 * 60 * 24) // token berlaku seharian, besok harus login ulang
+                    .autoApprove(true);
+        }
+    }
+
+}
