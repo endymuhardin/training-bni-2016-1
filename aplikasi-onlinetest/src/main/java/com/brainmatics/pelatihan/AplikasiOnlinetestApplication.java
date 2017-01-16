8016b23bc1b35d29ee8c1b071a1f272992c10595
Endy Muhardin <endy.muhardin@gmail.com>
2017-01-16 20:54:02 +0700

    enable resource server

--- aplikasi-onlinetest/src/main/java/com/brainmatics/pelatihan/AplikasiOnlinetestApplication.java
+++ aplikasi-onlinetest/src/main/java/com/brainmatics/pelatihan/AplikasiOnlinetestApplication.java
@@ -2,8 +2,10 @@ package com.brainmatics.pelatihan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class AplikasiOnlinetestApplication {
 
 	public static void main(String[] args) {
