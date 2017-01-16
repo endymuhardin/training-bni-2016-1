8ad4bc5bc23575a77658a82a2d5dc864ecea6972
Endy Muhardin <endy.muhardin@gmail.com>
2017-01-16 20:30:39 +0700

    starter project

---
+++ aplikasi-onlinetest/src/main/java/com/brainmatics/pelatihan/AplikasiOnlinetestApplication.java
@@ -0,0 +1,12 @@
+package com.brainmatics.pelatihan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiOnlinetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiOnlinetestApplication.class, args);
+	}
+}
