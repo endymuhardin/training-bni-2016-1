88cb5be9bdb9599073914688a36f1f33dbb75c36
Endy Muhardin <endy.muhardin@gmail.com>
2017-01-16 21:02:32 +0700

    mengatur permission untuk mengakses method

--- aplikasi-onlinetest/src/main/java/com/brainmatics/pelatihan/controller/HasilTestController.java
+++ aplikasi-onlinetest/src/main/java/com/brainmatics/pelatihan/controller/HasilTestController.java
@@ -3,6 +3,7 @@ package com.brainmatics.pelatihan.controller;
 import com.brainmatics.pelatihan.dao.HasilTestDao;
 import com.brainmatics.pelatihan.entity.HasilTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class HasilTestController {
     private HasilTestDao hasilTestDao;
     
     @RequestMapping("/hasil/{peserta}/")
+    @PreAuthorize("hasAuthority('EDIT_PESERTA')")
     public Iterable<HasilTest> hasilByPeserta(@PathVariable("peserta") String peserta){
         return hasilTestDao.findByPeserta(peserta);
     }
