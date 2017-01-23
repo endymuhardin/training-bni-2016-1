69f60aa62e8941e45d6f85e825f4ef2104bb82b9
Endy Muhardin <endy.muhardin@gmail.com>
2017-01-23 17:36:37 +0700

    starter app

---
+++ aplikasi-pelatihan-angularjs/src/main/java/com/brainmatics/pelatihan/AplikasiPelatihanAngularjsApplication.java
@@ -0,0 +1,12 @@
+package com.brainmatics.pelatihan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiPelatihanAngularjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiPelatihanAngularjsApplication.class, args);
+	}
+}
