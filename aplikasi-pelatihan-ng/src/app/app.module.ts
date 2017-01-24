1d6f21d6db3fc04281d25d4424779f0b054af05a
Endy Muhardin <endy.muhardin@gmail.com>
2017-01-24 21:09:06 +0700

    implement routing sederhana

--- aplikasi-pelatihan-ng/src/app/app.module.ts
+++ aplikasi-pelatihan-ng/src/app/app.module.ts
@@ -2,19 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Route }   from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 import { PesertaModule } from './peserta/peserta.module';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+const ROUTES: Route[] = [
+	{path: 'about', component: AboutComponent},
+	{path: '**', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+	RouterModule.forRoot(ROUTES),
 	PesertaModule
   ],
   providers: [],
